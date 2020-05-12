f7d4e1d570e02bfa61824b806f25933d7d0a7353
Inikon <ZhanZS@mail.ru>
2020-05-12 21:42:40 +0300

    fix: create function for many plane models

--- src/gl-canvas.component.js
+++ src/gl-canvas.component.js
@@ -56,49 +56,52 @@ class GLCanvas extends Control{
 
     let fragmentShaderSource =`
       precision mediump float;
+      uniform vec4 u_color;
       void main() {
-        gl_FragColor = vec4(1, 0, 0.5, 1);
+        gl_FragColor = u_color;
       }
     `;
     
     let gl = this.context;
-    var vertexShader = GLUtils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    var fragmentShader = GLUtils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-    var program = GLUtils.createProgram(gl, vertexShader, fragmentShader);
 
+    let program = makeShader(gl, vertexShaderSource, fragmentShaderSource);
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    
+    var colorLocation = gl.getUniformLocation(program, "u_color");
     var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-   /* var positions = [
-      0, 0, 0,
-      0, 0.5, 0,
-      0.4, 0, 0
-    ];*/
-    var positions = makeHorizontalPlaneModel({x:0, y:0}, {x:10.5, y:10.4}, -1);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-      // Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
-      var size = 3;          // 2 компоненты на итерацию
-      var type = gl.FLOAT;   // наши данные - 32-битные числа с плавающей точкой
-      var normalize = false; // не нормализовать данные
-      var stride = 0;        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
-      var offset = 0;        // начинать с начала буфера
-      gl.vertexAttribPointer(
-      positionAttributeLocation, size, type, normalize, stride, offset);
-
-      var primitiveType = gl.TRIANGLES;
-      var offset = 0;
-      var count = positions.length / size;
-
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.useProgram(program);
     gl.enableVertexAttribArray(positionAttributeLocation);
+
+
+var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var positions = makeHorizontalPlaneModel({x:0, y:0}, {x:10.5, y:10.4}, -1);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+/*
+    var positionBuffer1 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer1);
+
+    var positions1 = makeHorizontalPlaneModel({x:20, y:20}, {x:30.5, y:30.4}, -1);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions1), gl.STATIC_DRAW);
+*/  
+    let modelList = [];
+    for (let i = 0; i<10; i++){
+      let model = makeModel(
+        gl, 
+        makeHorizontalPlaneModel({x:0 + i*15, y:0}, {x:10.5+ i*15, y:10.4}, -1),
+        {r:Math.random()*100+100, g:Math.random()*100+100, b:Math.random()*100+100, a:255},
+        renderModel
+      );
+      modelList.push(model);
+      console.log(model);
+    }
+    
+
+   
 
 
 
@@ -116,13 +119,56 @@ class GLCanvas extends Control{
       var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
       var matrix = makeCameraMatrix(aspect, this.camRX, this.camRY, this.posX, this.posY, 0);
       gl.uniformMatrix4fv(matrixLocation, false, matrix);
-      
-      gl.drawArrays(primitiveType, offset, count);
+
+     // renderModel(gl, positionBuffer, positionAttributeLocation, {r:100, g:100, b:200, a:255}, colorLocation);
+      //renderModel(gl, positionBuffer1, positionAttributeLocation, {r:200, g:100, b:200, a:255}, colorLocation);
+      modelList.forEach(it=>{
+        //it.render(gl, it.positionBuffer, positionAttributeLocation, it.color, colorLocation);
+        renderModel(gl, it.positionBuffer, 6 , positionAttributeLocation, it.color, colorLocation);
+      });
+
       requestAnimationFrame(drawScene);
     }
     requestAnimationFrame(drawScene);
     
   }
+}
+
+function makeModel(gl, vertexList, color, renderFunction){
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexList), gl.STATIC_DRAW);  
+  return {
+    positionBuffer,
+    color
+  }
+}
+
+/*function setCamera(gl, matrixLocation){
+  var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  var matrix = makeCameraMatrix(aspect, this.camRX, this.camRY, this.posX, this.posY, 0);
+  gl.uniformMatrix4fv(matrixLocation, false, matrix);
+}*/
+
+function renderModel(gl, glBuffer, bufLength, positionAttributeLocation, color, colorLocation){
+
+  gl.uniform4f(colorLocation, color.r/255, color.g/255, color.b/255, color.a/255);
+  gl.bindBuffer(gl.ARRAY_BUFFER, glBuffer);
+
+  // Указываем атрибуту, как получать данные от positionBuffer (ARRAY_BUFFER)
+  var size = 3;          // 2 компоненты на итерацию
+  var type = gl.FLOAT;   // наши данные - 32-битные числа с плавающей точкой
+  var normalize = false; // не нормализовать данные
+  var stride = 0;        // 0 = перемещаться на size * sizeof(type) каждую итерацию для получения следующего положения
+  var offset = 0;        // начинать с начала буфера
+  gl.vertexAttribPointer(
+  positionAttributeLocation, size, type, normalize, stride, offset);
+
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  //var count = positions.length / size;
+  var count = bufLength; 
+  gl.drawArrays(primitiveType, offset, count); 
 }
 
 function makeHorizontalPlaneModel(v1, v2, z){
@@ -146,6 +192,14 @@ function makeCameraMatrix(aspect, rx, ry, px, py, pz){
   matrix = m4.translate(matrix, px, py, pz);
   return matrix;
 }
+
+function makeShader(gl, vertexShaderSource, fragmentShaderSource){
+  var vertexShader = GLUtils.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  var fragmentShader = GLUtils.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  var program = GLUtils.createProgram(gl, vertexShader, fragmentShader);  
+  return program;
+}
+
 
 class Player{
   constructor (){
